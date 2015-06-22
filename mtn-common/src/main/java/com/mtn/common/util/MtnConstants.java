4162b8a4bc728e481f6723032a921a936e25a2cc
Prabhakar Manthena <prabhakar.sun97@gmail.com>
2015-06-23 00:08:50 +0530

    Added common constants.

---
+++ mtn-common/src/main/java/com/mtn/common/util/MtnConstants.java
@@ -0,0 +1,10 @@
+package com.mtn.common.util;
+
+/**
+ * @author Prabhakar Manthena
+ *
+ */
+public interface MtnConstants {
+
+	String PROPERTY_FILE="mnt.properties";
+}
--- mtn-common/src/main/java/com/mtn/common/util/PropertyUtil.java
+++ mtn-common/src/main/java/com/mtn/common/util/PropertyUtil.java
@@ -1,17 +1,40 @@
 package com.mtn.common.util;
 
+import java.io.InputStream;
 import java.util.Properties;
-
-
+/**
+ * @author Prabhakar Manthena
+ *
+ */
 public class PropertyUtil {
-	private Properties properties;
 
+	private static PropertyUtil propertyUtil;
+	
+	private Properties properties;
+	
+	private PropertyUtil() {
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream(MtnConstants.PROPERTY_FILE);
+        try {
+        	properties = new Properties();
+        	properties.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public synchronized static PropertyUtil getInstance() {
+		
+		if(propertyUtil == null)	
+		propertyUtil=new PropertyUtil();
+		
+		return propertyUtil;
+	}
+
+	/**
+	 * @return the properties
+	 */
+	
 	public Properties getProperties() {
 		return properties;
 	}
-
-	public void setProperties(Properties properties) {
-		this.properties = properties;
-	}
-	
 }
