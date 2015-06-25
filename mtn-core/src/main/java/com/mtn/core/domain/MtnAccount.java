a247c5192b0582d285cfffb6e9b66c3469bdfc34
Prabhakar Manthena <prabhakar.sun97@gmail.com>
2015-06-26 01:15:50 +0530

    Added account, module, modulerole hbm files and MtnAccount object.

---
+++ mtn-core/src/main/java/com/mtn/core/domain/MtnAccount.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.mtn.core.domain;
+
+import java.util.Date;
+
+/**
+ * @author Prabhakar Manthena
+ *
+ */
+public class MtnAccount {
+	private Integer accountId;
+	private String accountName;
+	private Date activeDate;
+	private Date expireDate;
+	private boolean isExpired;
+	private boolean isActive;
+	public Integer getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public Date getActiveDate() {
+		return activeDate;
+	}
+	public void setActiveDate(Date activeDate) {
+		this.activeDate = activeDate;
+	}
+	public Date getExpireDate() {
+		return expireDate;
+	}
+	public void setExpireDate(Date expireDate) {
+		this.expireDate = expireDate;
+	}
+	public boolean isExpired() {
+		return isExpired;
+	}
+	public void setExpired(boolean isExpired) {
+		this.isExpired = isExpired;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+}
